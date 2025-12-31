c2ff04a5113f496ab3955618a598c6228acaa1f4
yonghun213 <kun.bbqchicken@gmail.com>
2025-12-31 10:47:52 -0800

    Fix routes-manifest issue by setting distDir in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-// next.config.js
-module.exports = {
-  output: '.next',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Vercel 빌드에서 .next 디렉토리를 생성하도록 보장
+  distDir: '.next',
 };
+
+module.exports = nextConfig;
